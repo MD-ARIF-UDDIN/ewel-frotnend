5a74493d27cd4dbd09b1d37a6f805d65ffb25f09
MD Arif <mdarifuddin040@gmail.com>
2025-10-07 15:25:11 +0600

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     resolve: {
@@ -10,6 +9,9 @@ export default defineConfig({
             "@": path.resolve(__dirname, "./src"),
         },
     },
-})
-
-
+    build: {
+        outDir: 'dist',
+        sourcemap: false,
+    },
+    base: '/',
+});
